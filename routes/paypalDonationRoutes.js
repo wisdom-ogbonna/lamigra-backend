2f3a80b38fcab3996e6ad510c00b7132a8e6fbd7
wisdomfzolutions <wisdom@fzolutions.com>
2025-11-14 16:49:29 +0100

    all

--- routes/paypalDonationRoutes.js
+++ routes/paypalDonationRoutes.js
@@ -4,7 +4,7 @@ import express from "express";
 import {
   createPaypalDonation,
   capturePaypalDonation,
-} from "../controllers/paypaldonationController.js";
+} from "../controllers/paypalDonationController.js";
 
 const router = express.Router();
 
